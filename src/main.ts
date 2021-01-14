9c985884dfeb6fd3c2bf7b0cd8a1c7429f980f46
Philipp Giese <giese.philipp+git@gmail.com>
2021-01-14 14:22:30 +0100

    add typescript return type annotation

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,9 @@
-import Vue from "vue";
-import App from "./App.vue";
+import Vue from 'vue'
+import { VNode } from 'vue/types/umd'
+import App from './App.vue'
 
-Vue.config.productionTip = false;
+Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App)
-}).$mount("#app");
+  render: (h): VNode => h(App),
+}).$mount('#app')
